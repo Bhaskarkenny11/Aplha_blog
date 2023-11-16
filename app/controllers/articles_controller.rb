ac712cf149b35c7b78da4a497c3e68cd50a6a832
Bhaskar <bhaskarshines11@gmail.com>
2023-11-16 17:05:07 +0530

    in create added code to render plain text in routes added article.new route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,19 @@ class ArticlesController < ApplicationController
         @articles=Article.all
         
     end
+
+    def new 
+
+    end
+
+    def create 
+  
+   @article= Article.new(params.require(:article).permit(:title,:description))
+    render plain: @article.inspect
+end
+
+
+
+
+
 end
