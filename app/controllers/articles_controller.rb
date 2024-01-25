efafd5fc8e3410c8ccf72611f40dd6fcb7448dbf
Bhaskar <bhaskarshines11@gmail.com>
2024-01-25 12:41:22 +0530

    added edit and update actions and added edit erb file and added edit,destroy,in routes  and in main layout added flash notice method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,35 +1,46 @@
 class ArticlesController < ApplicationController
-    def about 
-    end
-    def show
-        #binding.pry
-        @article=Article.find(params[:id])
-    end
-
-    def index
-        @articles=Article.all
-        
-    end
-
-    def new 
-      @article=Article.new
-    end
-
-    def create 
-        @article= Article.new (article_params)
-        if  @article.save
-          redirect_to @article
-        else 
-        # puts @article.errors.inspect
-          render 'new',status: :unprocessable_entity
-        end
+  def about
   end
 
-def article_params
+  def show
+    #binding.pry
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new (article_params)
+    if @article.save
+      flash[:notice] = "Article created Successfully."
+      redirect_to @article
+    else
+      # puts @article.errors.inspect
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article updated successfully"
+      redirect_to @article
+    else
+      render "edit"
+    end
+  end
+
+  def article_params
     params.require(:article).permit(:title, :description)
   end
-
-
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   #Defines the root path route ("/")
-   #root "articles#index"
-   #resources :articles
-   #get 'about', to: 'articles#about'
-   get 'articles', to: 'articles#index', as: 'articles'
-   get 'about', to: 'articles#about', as: 'about'
-   resources :articles , only:[:show,:index,:new,:create,:edit,:destroy]
-  end
+  #root "articles#index"
+  #resources :articles
+  #get 'about', to: 'articles#about'
+  get "articles", to: "articles#index", as: "articles"
+  get "about", to: "articles#about", as: "about"
+  resources :articles, only: [:show, :index, :new, :create, :edit, :destroy, :update]
+end
