ce3cfa3bff07a005ef54ca4687a1352966846058
Bhaskar <bhaskarshines11@gmail.com>
2024-02-02 21:43:04 +0530

    added articles/home as  root_path in routes and modifed Alphablog text as link to root path in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   #Defines the root path route ("/")
-  root "articles#index"
+  root "articles#home"
   #resources :articles
   #get 'about', to: 'articles#about'
   get "articles", to: "articles#index", as: "articles"
