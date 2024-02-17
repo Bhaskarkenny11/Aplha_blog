f1df5ec313b74aaae9a7b2d4afffcf4936222c33
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-02-17 22:33:32 +0530

    Revert "added user_id to article via migration and added associations to both user and article via dev branch"
    This reverts commit dd748538e854805f08b247e5c2e3fbc355a387b4.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,4 @@
 class Article < ApplicationRecord
-  belongs_to :user
   validates :title, presence: true
   validates_length_of :title, minimum: 5
   validates :description, presence: true, length: { minimum: 4 }
