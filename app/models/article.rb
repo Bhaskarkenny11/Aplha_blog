dd748538e854805f08b247e5c2e3fbc355a387b4
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-02-17 22:20:21 +0530

    added user_id to article via migration and added associations to both user and article via dev branch

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true
   validates_length_of :title, minimum: 5
   validates :description, presence: true, length: { minimum: 4, maximum: 100 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_13_183005) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_17_161941) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_13_183005) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
