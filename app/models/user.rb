2ae4cd781ed57ac86960958c8ef72aae5379eedc
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-02-17 22:49:32 +0530

    added email validation again because it got deleted by another branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+    validates :email, presence: true, length: { maximum: 105 }
+  
+    validates :email, presence: true, length: { maximum: 105 }, 
+                                     uniqueness: {case_sensitive: false},
+                                     format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
