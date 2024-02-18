edd20f54a72c3b0c618efbe026887b50f7953ecd
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-02-18 10:29:46 +0530

    added original user for the article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new (article_params)
+    @article.user = User.first
     if @article.save
       flash[:notice] = "Article created Successfully."
       redirect_to @article
