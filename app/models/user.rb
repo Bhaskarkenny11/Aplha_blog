22023709b79c2fe7a2fa85d1c86e516b32aee81a
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-03-23 23:43:37 +0530

    added has secure password to model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    
     before_save {self.email=email.downcase}
     has_many :articles
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
@@ -7,4 +8,5 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 105 }, 
                                      uniqueness: {case_sensitive: false},
                                      format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+      has_secure_password
 end
