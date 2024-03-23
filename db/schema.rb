bd6db9d6be6a4605b62bfa9ea7efc67dd0d0b21f
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-03-23 20:43:47 +0530

    deleted previous passwd field

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_23_145924) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    
+    t.string "password"
     t.string "password_digest"
   end
 
