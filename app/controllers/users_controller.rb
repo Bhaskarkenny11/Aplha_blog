c3e7e1174a48aea79200ca6b885d2f7b834bed30
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-03-26 23:57:28 +0530

    creating new user via login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
 
     def new
-@user=User.new
+       @user=User.new
+    end
+
+    def create
+     #binding.pry
+     @user=User.new(user_params)
+     binding.pry
+        if @user.save 
+            binding.pry
+            flash[:notice]= "user Successfully created"
+            redirect_to :articles_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username,:email,:password)
     end
 
 end
